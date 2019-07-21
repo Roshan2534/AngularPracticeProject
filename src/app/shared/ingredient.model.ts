bef68bb8eb6093e97d0483ef3d55e57c1879dc7d
Your Name <you@example.com>
2019-07-21 20:13:20 +0530

    Second Commit

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+import { NodeCompatibleEventEmitter } from 'rxjs/internal/observable/fromEvent';
+
+export class Ingredient
+{
+  constructor(public name:string,public amount:number){}
+}
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('nameInput') nameInputRef:ElementRef;
+  @ViewChild('amountInput') amountInputRef:ElementRef;
+  @Output() IngredientAdded=new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem(){
+    const ingname=this.nameInputRef.nativeElement.vlaue;
+    const ingamount=this.amountInputRef.nativeElement.vlaue;
+    const newIngredient=new Ingredient(ingname,ingamount);
+    this.IngredientAdded.emit(newIngredient);
+  }
 }
